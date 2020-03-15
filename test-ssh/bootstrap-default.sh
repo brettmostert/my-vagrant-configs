08eeff5d5ba231f778c4feda13778f7146f63443
Brett Mostert <brettmostert@gmail.com>
2020-03-15 21:51:03 +0200

    hmm

--- test-ssh/bootstrap-default.sh
+++ test-ssh/bootstrap-default.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-#~/.ssh/id_rsa.pub
 
-echo $1
-echo $2
+echo "Username :" $1
+echo "Password :" $2
 
 sudo adduser $1 --gecos "Administrator" --disabled-password
 echo $1":"$2 | sudo chpasswd
 
 sudo usermod -aG sudo admin
-
-# echo -e "root\nroot" | sudo passwd root
-
-#Should really use keys here ... https://help.ubuntu.com/community/SSH/OpenSSH/Keys but anyways :)
-# sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-# sudo service ssh restart
